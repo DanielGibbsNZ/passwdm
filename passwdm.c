2295d8cc79bcd35a9d35f36f5d4e985d82aeda66
Daniel Gibbs <daniel@imezio.com>
2012-11-19 12:15:24 +1300

    Added basic open and close commands. They don't actually do anything yet.

--- passwdm.c
+++ passwdm.c
@@ -8,13 +8,16 @@
 
 void perform_command(char *);
 
+char *database = NULL;
+char *prompt = "> ";
+
 int main(int argc, char *argv[]) {
 	// Prevent TAB from auto-completing file names.
 	rl_bind_key('\t', rl_insert);
 
 	// Read commands from the user.
 	char *command;
-	while((command = readline("> ")) != NULL) {
+	while((command = readline(prompt)) != NULL) {
 		// Maintain a history of commands.
 		if(*command)
 			add_history(command);
@@ -32,7 +35,7 @@ int main(int argc, char *argv[]) {
 	// If EOF was received, output a new line.
 	if(command == NULL)
 		printf("\n");
-	
+
 	return 0;
 }
 
@@ -40,6 +43,40 @@ void perform_command(char *command) {
 	char *keyword = strtok(command, COMMAND_DELIMETERS);
 	if(keyword == NULL)
 		return;
+	// Open a password database.
+	else if(strcmp(keyword, "open") == 0) {
+		// Get the database name.
+		char *dbname = strtok(NULL, COMMAND_DELIMETERS);
+		char *arg = strtok(NULL, COMMAND_DELIMETERS);
+		if(arg != NULL) {
+			printf("Unknown argument: %s\n", arg);
+		}
+		else if(dbname == NULL) {
+			printf("Password database name required.\n");
+		}
+		else if(database != NULL) {
+			printf("Current password database must be closed before another one can be opened.\n");
+		}
+		// Change to the database and update the prompt.
+		else {
+			database = strdup(dbname);
+			asprintf(&prompt, "%s> ", database);
+		}
+	}
+	// Close the current password database.
+	else if(strcmp(keyword, "close") == 0) {
+		char *arg = strtok(NULL, COMMAND_DELIMETERS);
+		if(arg != NULL) {
+			printf("Unknown argument: %s\n", arg);
+		}
+		else if(database == NULL) {
+			printf("No password database currently open.\n");
+		}
+		else {
+			database = NULL;
+			prompt = "> ";
+		}
+	}
 	else {
 		printf("%s: command not found\n", keyword);
 	}
