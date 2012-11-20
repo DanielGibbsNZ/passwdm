b5afe19a805dfe9a9fa6de6ccf05b213885ff84a
Daniel Gibbs <daniel@imezio.com>
2012-11-20 14:19:39 +1300

    Added create command to create password databases. Tidied up code a little bit, and fixed a few mistakes.

--- passwdm.c
+++ passwdm.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <readline/readline.h>
@@ -11,9 +12,15 @@
 #define COMMAND_DELIMETERS " \t\r\n"
 
 void perform_command(char *);
+void create_database(char *);
+void open_database(char *);
+void close_database();
+
+char *db_dir = NULL;
 
 char *database = NULL;
 char *prompt = "> ";
+FILE *db_file = NULL;
 
 int main(int argc, char *argv[]) {
 	// Check that password database directory exists, and if not creates it.
@@ -22,12 +29,11 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "Unable to locate home directory.");
 		return 1;
 	}
-	char *config_dir;
-	if(asprintf(&config_dir, "%s/.passwdm", home_dir) == -1) {
+	if(asprintf(&db_dir, "%s/.passwdm", home_dir) == -1) {
 		fprintf(stderr, "Out of memory.\n");
 		return 1;
 	}
-	if(mkdir(config_dir, S_IRUSR | S_IWUSR | S_IXUSR) == -1 && errno != EEXIST) {
+	if(mkdir(db_dir, S_IRUSR | S_IWUSR | S_IXUSR) == -1 && errno != EEXIST) {
 		fprintf(stderr, "Unable to create password database directory.\n");
 		return 1;
 	}
@@ -52,6 +58,8 @@ int main(int argc, char *argv[]) {
 		free(command);
 	}
 
+
+	close_database();
 	// If EOF was received, output a new line.
 	if(command == NULL)
 		printf("\n");
@@ -63,15 +71,34 @@ void perform_command(char *command) {
 	char *keyword = strtok(command, COMMAND_DELIMETERS);
 	if(keyword == NULL)
 		return;
-	// Open a password database.
-	else if(strcmp(keyword, "open") == 0) {
+	// Create a password database.
+	else if(strcmp(keyword, "create") == 0) {
 		// Get the database name.
-		char *dbname = strtok(NULL, COMMAND_DELIMETERS);
+		char *db_name = strtok(NULL, COMMAND_DELIMETERS);
 		char *arg = strtok(NULL, COMMAND_DELIMETERS);
 		if(arg != NULL) {
 			printf("Unknown argument: %s\n", arg);
 		}
-		else if(dbname == NULL) {
+		else if(db_name == NULL) {
+			printf("Password database name required.\n");
+		}
+		else if(database != NULL) {
+			printf("Current password database must be closed before another one can be created.\n");
+		}
+		// Change to the database and update the prompt.
+		else {
+			create_database(db_name);
+		}
+	}
+	// Open a password database.
+	else if(strcmp(keyword, "open") == 0) {
+		// Get the database name.
+		char *db_name = strtok(NULL, COMMAND_DELIMETERS);
+		char *arg = strtok(NULL, COMMAND_DELIMETERS);
+		if(arg != NULL) {
+			printf("Unknown argument: %s\n", arg);
+		}
+		else if(db_name == NULL) {
 			printf("Password database name required.\n");
 		}
 		else if(database != NULL) {
@@ -79,24 +106,7 @@ void perform_command(char *command) {
 		}
 		// Change to the database and update the prompt.
 		else {
-			// Get passphrase for this database.
-			char *pass_prompt;
-			if(asprintf(&pass_prompt, "Passphrase for %s: ", dbname) == -1) {
-				printf("Out of memory.\n");
-				return;
-			}
-			char *passphrase = getpass(pass_prompt);
-			// Validate passphrase.
-			if(passphrase) {
-				database = strdup(dbname);
-				if(asprintf(&prompt, "%s> ", database) == -1) {
-					printf("Out of memory.\n");
-					return;
-				}
-			}
-			else {
-				printf("Invalid passphrase.\n");
-			}
+			open_database(db_name);
 		}
 	}
 	// Close the current password database.
@@ -109,11 +119,83 @@ void perform_command(char *command) {
 			printf("No password database currently open.\n");
 		}
 		else {
-			database = NULL;
-			prompt = "> ";
+			close_database();
 		}
 	}
 	else {
-		printf("%s: command not found\n", keyword);
+		printf("%s: command not found.\n", keyword);
 	}
+}
+
+void create_database(char *db_name) {
+	char *db_filename;
+	if(asprintf(&db_filename, "%s/%s", db_dir, db_name) == -1) {
+		printf("Out of memory.\n");
+		return;
+	}
+	int db_fd = open(db_filename, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+	if(db_fd == -1) {
+		if(errno == EEXIST) {
+			printf("%s: password database already exists.\n", db_name);
+			return;
+		}
+		else {
+			printf("%s: error creating password database.\n", db_name);
+			return;
+		}
+	}
+	FILE *db = fdopen(db_fd, "w");
+	if(db == NULL) {
+		printf("%s: error creating password database.\n", db_name);
+		return;
+	}
+	database = strdup(db_name);
+	if(asprintf(&prompt, "%s> ", database) == -1) {
+		printf("Out of memory.\n");
+		fclose(db);
+		return;
+	}
+	db_file = db;
+}
+
+void open_database(char *db_name) {
+	char *db_filename;
+	if(asprintf(&db_filename, "%s/%s", db_dir, db_name) == -1) {
+		printf("Out of memory.\n");
+		return;
+	}
+	FILE *db = fopen(db_filename, "rb");
+	if(db == NULL) {
+		printf("%s: password database not found.\n", db_name);
+		return;
+	}
+	// Get passphrase for this database.
+	char *pass_prompt;
+	if(asprintf(&pass_prompt, "Passphrase for %s: ", db_name) == -1) {
+		printf("Out of memory.\n");
+		fclose(db);
+		return;
+	}
+	char *passphrase = getpass(pass_prompt);
+	// Validate passphrase.
+	if(passphrase) {
+		database = strdup(db_name);
+		if(asprintf(&prompt, "%s> ", database) == -1) {
+			printf("Out of memory.\n");
+			fclose(db);
+			return;
+		}
+		db_file = db;
+	}
+	else {
+		printf("Invalid passphrase.\n");
+		fclose(db);
+	}
+}
+
+void close_database() {
+	if(db_file != NULL)
+		fclose(db_file);
+	prompt = "> ";
+	database = NULL;
 }
